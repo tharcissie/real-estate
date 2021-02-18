592be871e1ec4d1d75065a55055fad15295f1c0b
tharcissie <tharcissieidufashe@gmail.com>
2021-02-18 14:00:48 +0200

    created models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Sector, District, Profile, House
 
-# Register your models here.
+admin.site.register(Sector)
+admin.site.register(Profile)
+admin.site.register(District)
+admin.site.register(House)
