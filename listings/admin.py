aa6fe17ebe8650ff688e0de9bb17ddf7f11e2b44
Ange Uwimana <uuwange@gmail.com>
2021-02-26 19:03:22 +0200

    Fixed errors

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Listing
 
-# Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_data', 'manager')
+    list_display_links = ('id', 'title')
+    list_filter = ('manager',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description',
+                     'district', 'sector')
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
