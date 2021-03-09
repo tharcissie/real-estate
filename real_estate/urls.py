81e8fecc57b81ac6523f2e5d592e679a7e29ac66
tharcissie <tharcissieidufashe@gmail.com>
2021-03-09 12:17:25 +0200

    styles

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(),  name='logout'),
 ]
 if settings.DEBUG:
